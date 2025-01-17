99e1390a6995c5d1f4e4aaa0d1c1c593a2582e28
joaopefreitas <joao.freitas@faculdadecesusc.edu.br>
2025-01-17 16:47:53 -0300

    Adiciona sentry para produção

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 const {
     wrapWithReanimatedMetroConfig,
   } = require('react-native-reanimated/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 module.exports = wrapWithReanimatedMetroConfig(config);
